320243d9b80c2aa6fb83fd045048c388c57f00c1
Anna Marie Sterling <sterling.am@gmail.com>
2022-04-20 14:14:08 -0600

    Add destinations

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ array = ["Sterling", "Bryce", "Mayu", "Samuel", "Amy"]
 puts array[2]
 puts array[4]
 puts array[0]
+destinations = ["Italy", "Spain", "Ireland", "Djbouti", "Japan"]
+destinations.shift
+puts destinations
